c17ed104c720a38fde6451326a2a58b449c0845b
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-04-02 10:10:54 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Tips Keeper</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>
+                Esse é o Título
+            </h1>
+            <p>
+                Esse é a Paragrafo
+            </p>
+        </div>
+    )
+}
+
+export default Note;
